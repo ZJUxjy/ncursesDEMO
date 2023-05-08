0a81cd6993ba59359984cee989e4e72bda8e85bf
jingyao.xu <jingyao.xu@phigent.ai>
2023-05-08 11:26:34 +0800

    [style] format with google style

--- basics/hello_world.c
+++ basics/hello_world.c
@@ -1,12 +1,11 @@
 #include <ncurses.h>
 
-int main()
-{	
-	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
+int main() {
+  initscr();                 /* Start curses mode 		  */
+  printw("Hello World !!!"); /* Print Hello World		  */
+  refresh();                 /* Print it on to the real screen */
+  getch();                   /* Wait for user input */
+  endwin();                  /* End curses mode		  */
 
-	return 0;
+  return 0;
 }
--- basics/key_code.c
+++ basics/key_code.c
@@ -1,14 +1,14 @@
 #include <ncurses.h>
 
-int main()
-{	int ch;
+int main() {
+  int ch;
 
-	initscr();
-	cbreak();
-	noecho();
-	keypad(stdscr, TRUE);
+  initscr();
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
 
-	ch = getch();
-	endwin();
-	printf("The key pressed is %d\n", ch);
+  ch = getch();
+  endwin();
+  printf("The key pressed is %d\n", ch);
 }
